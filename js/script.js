22dfa830df011e47a87b98b8ed69f80c4b938457
t0ntin <115113636+t0ntin@users.noreply.github.com>
2023-01-15 13:20:36 -0500

    Added features, changed some styles
    Added a backspace button, made it so that only one operator appears on screen, Added .localeToString method to the result.

--- js/script.js
+++ js/script.js
@@ -4,40 +4,41 @@ const display1 = document.querySelector(".display1");
 const operationButtons = document.querySelectorAll(".operator");
 const numberButtons = document.querySelectorAll(".num-btn");
 const clearBtn = document.querySelectorAll("#clear-btn");
-const signChangeBtn = document.querySelectorAll("#sign-change-btn");
+const signChangeBtn = document.querySelector("#sign-change-btn");
+const backspaceBtn = document.querySelector(".backspace-btn");
 
 let operator = "";
 let num1 = "";
 let num2 = "";
 let result = 0;
-// let disabled = true;
-
+let operatorAdded = false;
 
 numberButtons.forEach((button) => {
   button.addEventListener("click", () => {
     if (button.innerText === "." && display2.value.includes(".")) {
-      return
+      return;
     }
     if (display2.value.length > 9) {
       return;
     }
-      display2.value += button.innerText;
+    display2.value += button.innerText;
   });
 });
 
 operationButtons.forEach((button) => {
   button.addEventListener("click", () => {
+    allowOnlyOneOperator(button);
     if (num1 == "") {
       operator = button.innerText;
       num1 = display2.value;
       display1.innerText += num1;
       display1.innerText += operator;
-    console.log("num1 at opbuttons = " + num1);
+      console.log("num1 at opbuttons = " + num1);
       display2.value = "";
     } else {
-        num2 = display2.value;
-        operate(operator, num1, num2);
-        operator = button.innerText;
+      num2 = display2.value;
+      operate(operator, num1, num2);
+      operator = button.innerText;
       display1.innerText += button.innerText;
       console.log(num2);
       console.log(result);
@@ -51,65 +52,65 @@ operationButtons.forEach((button) => {
 });
 
 equalsBtn.addEventListener("click", () => {
-    num2 = display2.value;
-    display2.innerText = num2;
-    console.log("num2 at result " +num2);
-    let check = operate(operator, num1, num2);
-    if(check === undefined){
-        display2.value = "";
-    }else{
-        display2.value = check;
-    }
-    operator = ""; 
-    num2 = "";  
+  num2 = display2.value;
+  display2.innerText = num2;
+  console.log("num2 at result " + num2);
+  let check = operate(operator, num1, num2);
+  if (check === undefined) {
+    display2.value = "";
+  } else {
+    display2.value = check;
+  }
+  operator = "";
+  num2 = "";
 });
 
 clearBtn.forEach((button) => {
   button.addEventListener("click", () => {
-  clearCalculator();
+    clearCalculator();
   });
 });
 
-signChangeBtn.forEach((button) => {
-  button.addEventListener("click", () => {
-      if (display2.value > 0) {
-        display2.value = display2.value * -1;
-        console.log(display2.value);
-      } else if (display2.value.includes("-")){
-        display2.value = Math.abs(display2.value);
-      }
-  });
+signChangeBtn.addEventListener("click", () => {
+  if (display2.value > 0) {
+    display2.value = display2.value * -1;
+    console.log(display2.value);
+  } else if (display2.value.includes("-")) {
+    display2.value = Math.abs(display2.value);
+  }
+});
+
+backspaceBtn.addEventListener("click", () => {
+  display2.value = display2.value.slice(0, -1);
 });
 
 const operate = (operator, num1, num2) => {
-    num1 = parseFloat(num1);
-    num2 = parseFloat(num2);
-    console.log("operator at operate = " + operator);
-
-    console.log("num1 at operate = " + num1);
-    console.log("num2 at operate = " + num2);
+  num1 = parseFloat(num1);
+  num2 = parseFloat(num2);
+  console.log("operator at operate = " + operator);
+  console.log("num1 at operate = " + num1);
+  console.log("num2 at operate = " + num2);
   if (operator === "+") {
-    return result = roundResult((num1 + num2));
+    return (result = roundResult(num1 + num2).toLocaleString());
   } else if (operator === "-") {
-    return result = roundResult((num1 - num2));
+    return (result = roundResult(num1 - num2).toLocaleString());
   } else if (operator === "x") {
-    return result = roundResult((num1 * num2));
-  } else if (operator === "÷" && num2 ===0) {
-    display1.innerText = "ERROR"    
+    return (result = roundResult(num1 * num2).toLocaleString());
+  } else if (operator === "÷" && num2 === 0) {
+    display1.innerText = "ERROR";
     console.log("num1 after error found: " + num1);
-    operator = "÷"
-    return 
+    operator = "÷";
+    return;
   } else if (operator === "÷") {
-    return result = roundResult((num1 / num2));
-  }
-   else {
+    return (result = roundResult(num1 / num2).toLocaleString());
+  } else {
     console.log("Invalid operator");
     return;
   }
-}
+};
 
 function roundResult(number) {
-  return Math.round(number * 1000) / 1000
+  return Math.round(number * 1000) / 1000;
 }
 
 function clearCalculator() {
@@ -120,25 +121,22 @@ function clearCalculator() {
   display1.innerText = "";
 }
 
-
-// function disableEqualsBtn() {
-//   if (!operator) {
-//     equalsBtn.disabled = disabled;
-//   } else {
-//     equalsBtn.disabled = !disabled;
-//   }
-// }
-// disableEqualsBtn();
+function allowOnlyOneOperator(button) {
+  if (!operatorAdded) {
+    display1.value += button.textContent;
+    operatorAdded = true;
+  }
+}
 
 window.addEventListener("keydown", function (e) {
   e.preventDefault();
   const number = document.querySelector(`button[data-key="${e.keyCode}"]`);
   if (!number) return;
-  number.click()
-})
+  number.click();
+});
 
 window.addEventListener("keydown", function (e) {
   const number2 = document.querySelector(`button[data-key2="${e.keyCode}"]`);
   if (!number2) return;
-  number2.click()
-})
+  number2.click();
+});
